dd0a8b70bbcc714ce174b12e88cd1c7c0cb2adef
Katherine <kfbrenes14@gmail.com>
2025-06-01 03:12:13 -0600

    Cambios locales antes de pull por problemas de merge

--- src/app/services/rutina/rutina.service.ts
+++ src/app/services/rutina/rutina.service.ts
@@ -4,15 +4,26 @@ import { HttpClient } from '@angular/common/http';
 import { Rutina } from '@app/domain/rutina.model';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
+import { RutinaCompletaDTO } from '@app/domain/dto/RutinaCompletaDTO';
+
 
 @Injectable({ providedIn: 'root' })
 export class RutinaService {
   private apiUrl = `${environment.apiBaseUrl}/clientes`;
   constructor(private http: HttpClient) { }
 
-  /** GET /api/clientes/{id}/rutinas */
-  obtenerPorCliente(idCliente: number): Observable<Rutina[]> {
-    return this.http.get<Rutina[]>(`${this.apiUrl}${idCliente}/rutinas`);
+  obtenerPorCliente(idCliente: number): Observable<RutinaCompletaDTO[]> {
+    return this.http.get<RutinaCompletaDTO[]>(`${environment.apiBaseUrl}/clientes/${idCliente}/rutinas`);
+  }
+
+
+  /** GET /api/clientes/{idCliente}/rutinas/{idRutina} */
+  obtenerRutinaPorId(idCliente: number, idRutina: number): Observable<Rutina> {
+    return this.http.get<Rutina>(`${this.apiUrl}/${idCliente}/rutinas/${idRutina}`);
+  }
+
+  obtenerTodas(): Observable<RutinaCompletaDTO[]> {
+    return this.http.get<RutinaCompletaDTO[]>(`${environment.apiBaseUrl}/rutinas`);
   }
 
   getByCliente(idCliente: number): Observable<Rutina[]> {
@@ -36,6 +47,20 @@ export class RutinaService {
 
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  update(rutina: RutinaCompletaDTO): Observable<RutinaCompletaDTO> {
+    const idCliente = rutina.cliente?.idPersona;
+    const idRutina = rutina.idRutina;
+
+    if (!idCliente || !idRutina) {
+      throw new Error('Cliente o ID de rutina faltante para actualizar.');
+    }
+
+    return this.http.put<RutinaCompletaDTO>(
+      `${this.apiUrl}/${idCliente}/rutinas/${idRutina}`,
+      rutina
+    );
   }
 
   /** Método para descargar el PDF de la rutina */
