71b8703689d9b9a5a351ab23048345796fb1b2c0
Katherine1407 <80642102+Katherine1407@users.noreply.github.com>
2025-06-02 15:00:18 -0600

    Push prueba vercel 7

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://94e1-163-178-107-104.ngrok-free.app/bulk-gym/api' //Linea de ejemplo
+  apiBaseUrl: 'https://3df8-163-178-107-104.ngrok-free.app/bulk-gym/api' //Linea de ejemplo
   
 };
