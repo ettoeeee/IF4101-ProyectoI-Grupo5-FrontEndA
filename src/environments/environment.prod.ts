42f635543ff140d610eb8fff92ee42bf67c2c308
ettoeeee <102703204+ettoeeee@users.noreply.github.com>
2025-06-03 21:21:14 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-//export const environment = {
- // production: true,
- /// apiBaseUrl: 'http://localhost:8080/bulk-gym/api'
-//};
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://if4101-proyectoi-grupo5a-production.up.railway.app/bulk-gym/api'
+};
